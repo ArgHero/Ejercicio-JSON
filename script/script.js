4d5d6c32f1d2258763e596e77bf82e1b720c7039
ArgHero <saul.hernandez.ipn@gmail.com>
2025-04-09 14:26:59 -0600

    Pagina de bienvenida

--- script/script.js
+++ script/script.js
@@ -1,23 +1,43 @@
-btnUsuario = document.getElementsByClassName("btn").item(0);
-txtNombre = document.getElementById("txtNombre");
-msgError = document.getElementById("msgError");
+let btnUsuario = document.getElementById("guardar");
+let btnBorrar = document.getElementById("eliminar");
+let txtNombre = document.getElementById("txtNombre");
+let msgError = document.getElementById("msgError");
 
+let title = document.getElementsByClassName("bienvenida").item(0);
+
+
+let usuario = localStorage.getItem("usuario");
+
+if(title){
+    if(usuario)
+        title.innerText = `Hola, ${usuario} bienvenido de nuevo.`;
+    else
+        title.innerText = "Por favor ve al index e ingresa tu nombre";
+}
+
+if(btnBorrar){
+    btnBorrar.addEventListener("click",function(event){
+        localStorage.removeItem("usuario");
+    });//btnBorrar
+}
 
 if(msgError){
     msgError.style.display = 'none';
     msgError.style.color = 'red';
 }
-
-btnUsuario.addEventListener("click",function(event){
-    event.preventDefault();
-    let usuario = txtNombre.value.trim();
-    if(usuario!=''){
-        localStorage.setItem("usuario",usuario);
-        txtNombre.value = "";
-        msgError.style.display = 'none';
-        window.location.href="./welcome.html";
-
-    }else
-        msgError.style.display = 'block';
+if(btnUsuario){
+    btnUsuario.addEventListener("click",function(event){
+        event.preventDefault();
+        let usuario = txtNombre.value.trim();
+        if(usuario!=''){
+            localStorage.setItem("usuario",usuario);
+            txtNombre.value = "";
+            msgError.style.display = 'none';
+            window.location.href="./welcome.html";
     
-});//btn
+        }else
+            msgError.style.display = 'block';
+        
+    });//btn
+};//existe btn
+
